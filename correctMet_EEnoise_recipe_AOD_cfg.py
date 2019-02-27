86ebbdefcb41f7a8d4ec9fd923af6c7ec13f81c0
Aimilios <aimilios.ioannou@cern.ch>
2019-02-27 16:06:45 +0100

    added file

---
+++ correctMet_EEnoise_recipe_AOD_cfg.py
@@ -0,0 +1,10 @@
+# This configuration uses a "modified" version of the EE noise recipe
+# for AOD step.
+#------------------------------------------------------------------------------
+import FWCore.ParameterSet.Config as cms
+
+import FWCore.ParameterSet.VarParsing as VarParsing
+options = VarParsing.VarParsing('analysis')
+
+
+
